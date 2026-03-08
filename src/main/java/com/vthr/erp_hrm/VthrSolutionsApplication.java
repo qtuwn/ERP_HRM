e867ad61140709af1fcd2d01c43cf4b3550a1f86
qtuwn <tuquoctuan201@gmail.com>
2026-03-08 16:58:46 +0700

    Initial project setup - Spring Boot ERP HRM

---
+++ src/main/java/com/vthr/erp_hrm/VthrSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.vthr.erp_hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VthrSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VthrSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vthr/erp_hrm/VthrSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vthr.erp_hrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VthrSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
